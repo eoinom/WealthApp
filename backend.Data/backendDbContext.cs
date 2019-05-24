e7995369f1ed553855318ae570e54464df72cb4b
Eoin <eoinom@gmail.com>
2019-05-24 20:59:13 +0100

    Update DbContext

--- backend.Data/backendDbContext.cs
+++ backend.Data/backendDbContext.cs
@@ -1,15 +1,26 @@
 using Microsoft.EntityFrameworkCore;
-using System;
+using backend.Data.Models;
 
 namespace backend.Data
 {
   public class backendDbContext : DbContext
   {
-    public backendDbContext(DbContextOptions options) : base(options)
-    {
+    public backendDbContext(DbContextOptions options) : base(options) { }
 
-    }
-
-    
+    public DbSet<AccountValue> AccountValues { get; set; }
+    public DbSet<BankAccount> BankAccounts { get; set; }
+    public DbSet<CashAccount> CashAccounts { get; set; }
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<CreditCard> CreditCards { get; set; }
+    public DbSet<CryptoAccount> CryptoAccounts { get; set; }
+    public DbSet<CryptoAccountValue> CryptoAccountValues { get; set; }
+    public DbSet<Cryptocurrency> Cryptocurrencies { get; set; }
+    public DbSet<CryptoValue> CryptoValues { get; set; }
+    public DbSet<Currency> Currencies { get; set; }
+    public DbSet<LoanAccount> LoanAccounts { get; set; }
+    public DbSet<MortgageAccount> MortgageAccounts { get; set; }
+    public DbSet<Property> Properties { get; set; }
+    public DbSet<PropertyValue> PropertyValues { get; set; }
+    public DbSet<User> Users { get; set; }
   }
 }
