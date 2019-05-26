6ba274725477ba8e1d4195e4942bd4bb1db8d9dc
Eoin <eoinom@gmail.com>
2019-05-26 01:26:33 +0100

    Add SeedData class with some Users seed data

---
+++ backend/backendData/backendDbSeedData.cs
@@ -0,0 +1,30 @@
+﻿using backendData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace backendData
+{
+    public static class backendDbSeedData
+    {
+        public static void EnsureSeedData(this backendDbContext db)
+        {
+            if ( !db.Users.Any() )
+            {
+                var users = new List<User>
+                {
+                    new User { Email = "eoinom@gmail.com", Password = "pass123", FirstName = "Eoin", LastName = "O'Malley" },
+                    new User { Email = "joe.bloggs@gmail.com", Password = "pass123", FirstName = "Joe", LastName = "Bloggs" },
+                    new User { Email = "paulmcc@applerecords.com", Password = "pass123", FirstName = "Paul", LastName = "McCartney" },
+                    new User { Email = "john@imagine.com", Password = "pass123", FirstName = "John", LastName = "Lennon" },
+                    new User { Email = "george@altavista.com", Password = "pass123", FirstName = "George", LastName = "Harrisson" },
+                    new User { Email = "ringo@drummers4hire.com", Password = "pass123", FirstName = "Ringo", LastName = "Starr" }
+                };
+
+                db.Users.AddRange(users);
+                db.SaveChanges();
+            }
+        }
+    }
+}
