1aa33e7915e55930000969291d0032dc7a37a11e
Eoin <eoinom@gmail.com>
2019-05-29 23:59:00 +0100

    Add MainCurrency to CountryType

--- backend/backendAPI/Types/CountryType.cs
+++ backend/backendAPI/Types/CountryType.cs
@@ -9,10 +9,11 @@ namespace backendAPI.Types
         {
             Name = "Country";
             Description = "A country in the world.";
-            Field(d => d.Iso2Code, nullable: false).Description("The ISO 3166-1 Alpha-2 code of the country.");
-            Field(d => d.Iso3Code, nullable: true).Description("The ISO 3166-1 Alpha-3 code of the country.");
-            Field(d => d.Name, nullable: true).Description("The name of the country.");
-            Field(d => d.Continent, nullable: true).Description("The Continent the country is part of.");
+            Field(x => x.Iso2Code, nullable: false).Description("The ISO 3166-1 Alpha-2 code of the country.");
+            Field(x => x.Iso3Code, nullable: true).Description("The ISO 3166-1 Alpha-3 code of the country.");
+            Field(x => x.Name, nullable: true).Description("The name of the country.");
+            Field(x => x.Continent, nullable: true).Description("The Continent the country is part of.");
+            Field(x => x.MainCurrency, nullable: true, type: typeof(CurrencyType)).Description("The main currency of the country.");
         }
     }
 }
--- backend/backendData/Models/Country.cs
+++ backend/backendData/Models/Country.cs
@@ -2,13 +2,13 @@ using System.ComponentModel.DataAnnotations;
 
 namespace backendData.Models
 {
-  public class Country
-  {
-    [Key]
-    public string Iso2Code { get; set; }
-
-    public string Iso3Code { get; set; }
-    public string Name { get; set; }
-    public string Continent { get; set; }
-  }
+    public class Country
+    {
+        [Key]
+        public string Iso2Code { get; set; }
+        public string Iso3Code { get; set; }
+        public string Name { get; set; }
+        public string Continent { get; set; }
+        public Currency MainCurrency { get; set; }
+    }
 }
