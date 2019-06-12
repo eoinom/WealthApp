796f2ea895e025460c3e95758451c140b92f2526
Eoin <eoinom@gmail.com>
2019-06-12 21:20:55 +0100

    Updates to Vuex store for storing user and bank accounts + values with mutations, actions and getters

--- frontend/src/store/store-main.js
+++ frontend/src/store/store-main.js
@@ -1,60 +1,130 @@
 import Vue from 'vue'
 
-const state = {
-    authenticated: false,
-    user: {}
-    // tasks: {
-    //     'ID1': {
-    //         name: "Go to shop",
-    //         completed: false,
-    //         dueDate: '2019/05/12',
-    //         dueTime: '18:30'
-    //     },
-    //     'ID2': {
-    //         name: "Get bananas",
-    //         completed: false,
-    //         dueDate: '2019/05/13',
-    //         dueTime: '14:00'
-    //     },
-    //     'ID3': {
-    //         name: "Get apples",
-    //         completed: false,
-    //         dueDate: '2019/05/14',
-    //         dueTime: '16:00'
-    //     }
-    // }
+const getDefaultState = () => {
+    return {
+        authenticated: false,
+        user: {
+            userId: 0,
+            email: '',
+            firstName: '',
+            lastName: '',
+            newsletterSub: false,
+            country: {},
+            displayCurrency: {},
+        },
+        bankAccounts: {
+            '0': {
+                bankAccountId: 0,
+                accountName: 'accountName',
+                description: 'description',
+                institution: 'institution',
+                type: 'type',
+                isActive: false,
+                currencyCode: 'EUR',
+                balance: 0.00
+            }
+        },
+        accountValues: {
+            '0': {
+                accountValueId: 0,
+                date: "2000-01-01",
+                value: 1000.00,
+                bankAccountId: 0
+            }
+        }
+    }
 }
 
-const mutations = {
-    // updateTask(state, payload) {
-    //     Object.assign(state.tasks[payload.id], payload.updates)
-    // },
-    // deleteTask(state, id) {
-    //     Vue.delete(state.tasks, id)
-    // },
-    updateAuth(state, payload) {
-        Object.assign(state.authenticated, payload.authenticated)
-    }
+// initial state (ref: https://tahazsh.com/vuebyte-reset-module-state)
+// **CHECK IF THIS IS REACTIVE, IF NOT LOOK AT: https://github.com/vuejs/vuex/issues/1118
+const state = getDefaultState()
+
+const mutations = {    
+    resetState (state) {
+        Object.assign(state, getDefaultState())
+    },
+    updateUser(state, user) {
+        Object.assign(state.user, user)
+    },
+    updateAuth(state, authenticated) {
+        state.authenticated = authenticated
+    },
+    initialiseBankAccounts(state, bankAccounts) {
+        // Vue.delete(state.bankAccounts, 0)
+        Object.assign(state.bankAccounts, bankAccounts)
+        // for (var i = 0; i < bankAccounts.size(); i++) {
+        //     Object.assign(state.bankAccounts[bankAccounts[i].bankAccountId], bankAccount[i])
+        // }
+        // for (var i = 0; i < bankAccounts.size(); i++) {
+        //     commit('addBankAccount', bankAccounts[i])
+        // }
+    },
+    addBankAccount(state, bankAccount) {
+        Object.assign(state.bankAccounts[bankAccount.bankAccountId], bankAccount)
+    },
+    deleteBankAccount(state, bankAccountId) {
+        // Vue.delete(state.bankAccounts, { where: bankAccountId = accountId })
+        Vue.delete(state.bankAccounts, bankAccountId)
+    },
+    updateBankAccountBalances(state) {
+        // To do
+    },
+    updateBankAccountBalance(state, bankAccountId) {
+        // To do
+    },
+    initialiseAccountValues(state, accountValues) {
+        commit('deleteAccountValue', 0)
+        Object.assign(state.accountValues, accountValues)
+    },
+    addAccountValue(state, accountValue) {
+        Object.assign(state.accountValues[accountValue.accountValueId], accountValue)
+    },
+    deleteAccountValue(state, accountValueId) {
+        // Vue.delete(state.bankAccounts, { where: bankAccountId = accountId })
+        Vue.delete(state.accountValues, accountValueId)
+    },
 }
 
 const actions = {
-    // updateTask({ commit }, payload) {
-    //     commit('updateTask', payload)
-    // },
-    deleteTask({ commit }, id) {
-        commit('deleteTask', id)
-    }
+    logout ({ commit }) {
+        commit('resetState')
+    },
+    login ({ commit }, payload) {
+        commit('updateUser', payload.user)
+        commit('initialiseBankAccounts', payload.bankAccounts)
+        commit('initialiseAccountValues', payload.accountValues)
+        commit('updateAccountBalances')
+    },
+    updateUser({ commit }, user) {
+        commit('updateUser', user)
+        commit('updateAuth', true)
+    },
+    initialiseBankAccounts({ commit }, bankAccounts) {
+        commit('initialiseBankAccounts', bankAccounts)
+    },
 }
 
 const getters = {
-    // tasks: (state) => {
-    //     return state.tasks
-    // },
     authenticated: (state) => {
         return state.authenticated
     },
     user: (state) => {
         return state.user
+    },
+    bankAccounts: (state) => {
+        return state.bankAccounts
+    },
+    bankAccountById: (state) => (id) => {
+        return state.bankAccounts.find(x => x.bankAccountId === id)
+    },
+    accountValues: (state) => {
+        return state.accountValues
+    },
+    accountValueById: (state) => (id) => {
+        return state.accountValues.find(x => x.accountValueId === id)
+    },
+    accountValuesByAccountId: (state) => (id) => {
+        return state.accountValues.where(x => x.bankAccountId === id)
     },
 }
 
