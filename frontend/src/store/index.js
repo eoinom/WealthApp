01b9e55c2bbe0ea1ebacf200416c71ced7ca586a
Eoin <eoinom@gmail.com>
2019-07-21 15:42:12 +0100

    Added vuex-persistedstate package to persist the user data on page refresh

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 import main from './main'
 import accounts from './accounts'
@@ -20,6 +21,7 @@ export default function (/* { ssrContext } */) {
       accounts,
       loans 
     },
+    plugins: [createPersistedState()],
 
     // enable strict mode (adds overhead!)
     // for dev mode only
