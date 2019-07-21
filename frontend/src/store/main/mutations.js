dd63cdfa815309aff58d9df79ccf29f30147904e
Eoin <eoinom@gmail.com>
2019-07-21 12:09:04 +0100

    Reset all Vuex stores on logout

--- frontend/src/store/main/mutations.js
+++ frontend/src/store/main/mutations.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import index from './index'
+import accountsIndex from '../accounts/index'
+import loansIndex from '../loans/index'
 
 function resetState (state) {  
-  // https://stackoverflow.com/questions/42295340/how-to-clear-state-in-vuex-store
-  Object.assign(state, index.getDefaultState())
+  // https://stackoverflow.com/questions/42295340/how-to-clear-state-in-vuex-store  
+  Object.assign(state, index.getDefaultState()) 
+  Object.assign(accountsIndex.state, accountsIndex.getDefaultState())
+  Object.assign(loansIndex.state, loansIndex.getDefaultState())
   // Vuex.store.replaceState({})
 }
 
@@ -12,7 +16,7 @@ function updateUser (state, user) {
 }
 
 function updateAuth (state, authenticated) {
-  state.authenticated = authenticated
+  state.authenticated = authenticated  
 }
 
 function updateTotalNetWorthBalances (state, totalNetWorthBals) {
