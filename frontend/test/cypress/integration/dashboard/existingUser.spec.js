bd8f63a5f71d652e95cb399e1a0433e711c45b1c
Eoin <eoinom@gmail.com>
2019-07-28 23:45:44 +0100

    Integration / E2E tests in Cypress for login, register and page navigation

---
+++ frontend/test/cypress/integration/dashboard/existingUser.spec.js
@@ -0,0 +1,106 @@
+/// <reference types="cypress" />
+
+import * as ctx from  '../../../../quasar.conf.js'
+import Chance from 'chance'
+const chance = new Chance()
+
+
+describe('Dashboard page tests for Existing User', () => {
+
+  const existing_email = "eoinom@gmail.com"
+  const existing_pass = "pass1234"
+  
+  beforeEach(() => {
+    // Login with custom method (using existing email & password)
+    cy.login(existing_email, existing_pass)
+    cy.wait(2000)
+  })
+
+  it('can visit Accounts from Dashboard on desktop', () => {
+    cy.visit('/dashboard')    
+    
+    // Set desktop viewport (should have burger menu button)
+    cy.viewport('macbook-15')
+    cy.wait(200)
+    
+    cy.get('.menuBtn').click()
+    cy.contains('Accounts').click()
+    cy.location('pathname').should('eq', '/accounts')
+
+    cy.go('back')
+    cy.location('pathname').should('eq', '/dashboard')    
+  })
+
+  it('can visit Loans from Dashboard on desktop', () => {
+    cy.visit('/dashboard')    
+    
+    // Set desktop viewport (should have burger menu button)
+    cy.viewport('macbook-15')
+    cy.wait(200)
+    
+    cy.get('.menuBtn').click()
+    cy.contains('Loans').click()
+    cy.location('pathname').should('eq', '/loans')
+
+    cy.go('back')
+    cy.location('pathname').should('eq', '/dashboard')    
+  })
+
+  it('can visit Accounts from Dashboard on mobile', () => {
+    cy.visit('/dashboard')    
+    
+    // Set mobile viewport (should have tab nav menu)
+    cy.viewport('iphone-6')
+    cy.wait(1000)
+    cy.get('footer').find('a').filter('.q-tab--inactive').first().children().find('i').should('contain', 'account')    
+    cy.get('footer').find('a').filter('.q-tab--inactive').first().click()
+    cy.location('pathname').should('eq', '/accounts')
+
+    cy.go(-1)
+    cy.location('pathname').should('eq', '/dashboard')   
+  })
+
+  it('can visit Loans from Dashboard on mobile', () => {
+    cy.visit('/dashboard')    
+    
+    // Set mobile viewport (should have tab nav menu)
+    cy.viewport('iphone-6')
+    cy.wait(1000)
+    cy.get('footer').find('a').filter('.q-tab--inactive').last().children().find('i').should('have.class', 'fas').and('have.class', 'fa-hand-holding-usd')
+    cy.get('footer').find('a').filter('.q-tab--inactive').last().click()
+    cy.location('pathname').should('eq', '/loans')
+
+    cy.go(-1)
+    cy.location('pathname').should('eq', '/dashboard')    
+  })
+
+  it('can visit Dashboard from Accounts on desktop', () => {
+    cy.visit('/accounts')    
+    
+    // Set desktop viewport (should have burger menu button)
+    cy.viewport('macbook-15')
+    cy.wait(200)
+    
+    cy.get('.menuBtn').click()
+    cy.contains('Dashboard').click()
+    cy.location('pathname').should('eq', '/dashboard')
+
+    cy.go('back')
+    cy.location('pathname').should('eq', '/accounts')    
+  })
+
+  it('can visit Loans from Dashboard on mobile', () => {
+    cy.visit('/accounts')    
+    
+    // Set mobile viewport (should have tab nav menu)
+    cy.viewport('iphone-6')
+    cy.wait(1000)
+    cy.get('footer').find('a').filter('.q-tab--inactive').children().find('i').should('have.class', 'fas').and('have.class', 'fa-chart-bar')
+    cy.get('footer').find('a').filter('.q-tab--inactive').first().click()
+    cy.location('pathname').should('eq', '/dashboard')
+
+    cy.go(-1)
+    cy.location('pathname').should('eq', '/accounts')    
+  })
+  
+});
---
+++ frontend/test/cypress/integration/login/login.spec.js
@@ -0,0 +1,107 @@
+/// <reference types="cypress" />
+
+import * as ctx from  '../../../../quasar.conf.js'
+import Chance from 'chance'
+const chance = new Chance()
+
+
+describe('Login page tests', () => {
+
+  const existing_email = "eoinom@gmail.com"
+  const existing_pass = "pass1234"
+  var countries = [ 'Australia', 'Canada', 'France', 'Germany', 'Ireland', 'Italy', 'Netherlands', 'Poland', 'Portugal', 'Spain', 'United Kingdomn', 'United States', 'New Zealand' ]
+  var currencyCodes = [ 'AUD', 'CAD', 'EUR', 'GBP', 'PLN', 'USD', 'NZD' ]
+  const new_email = chance.email()
+  const new_pass = chance.string({ length: 7 })
+  const new_fname = chance.first()
+  const new_lname = chance.last()
+  const new_country = chance.pickone(countries)
+  const new_currency = chance.pickone(currencyCodes)
+
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('.should() - assert that <title> is correct', () => {
+    cy.title().should('include', 'WealthApp.io')
+  })
+
+  it('contains `Login`', () => {
+    cy.contains('Login')
+  })
+
+  it('contains `Register`', () => {
+    cy.contains('Register')
+  })
+
+  it('has logo', () => {
+    cy.get('.authForm img')
+      .should('have.id', 'logo-auth')
+      .and('have.attr', 'src')
+      .and('match', /^(img\/Logo.png)/);
+  })
+
+  it('blocks going to dashboard without logging in', () => {
+    // cy.pause()
+    cy.visit('/dashboard')
+    cy.contains('Login')
+  })
+
+  
+  it('logs in existing user', () => {
+    // fill out login form
+    cy.get('input[type=email]').type(existing_email)
+    cy.get('input[type=password]').type(existing_pass)
+    cy.get('button[type=submit]').click()
+
+    // Assert welcome message
+    cy.contains('Welcome back')
+  })
+
+  it('registers new user', () => {
+    // go to register form
+    cy.get('.q-tab.q-tab--inactive').click()
+
+    // fill out register form
+    cy.get('#register-firstname').type(new_fname)
+    cy.get('#register-lastname').type(new_lname)
+    cy.get('#register-email').type(new_email)
+    cy.get('#register-password').type(new_pass)
+    cy.get('#register-confirmPassword').type(new_pass)
+    cy.get('#register-country').click()
+    cy.contains(new_country).click()
+    cy.get('#register-currency').click().click()    // why two clicks needed? 
+    cy.contains(new_currency).click()
+    cy.get('#register-agreeTerms .q-checkbox__check').click()
+    
+    // submit form
+    cy.get('button[type=register]').click()
+
+    // Assert welcome message
+    cy.contains('Welcome')
+  })
+
+})
+
+// describe('Home page tests', () => {
+//   beforeEach(() => {
+//     cy.visit('/');
+//   });
+//   it('has pretty background', () => {
+//     cy.get('.landing-wrapper')
+//       .should('have.css', 'background').and('match', /(".+(\/img\/background).+\.png)/);
+//   });
+//   it('has pretty logo', () => {
+//     cy.get('.landing-wrapper img')
+//       .should('have.class', 'logo-main')
+//       .and('have.attr', 'src')
+//       .and('match', /^(data:image\/svg\+xml).+/);
+//   });
+//   it('has very important information', () => {
+//     cy.get('.instruction-wrapper')
+//       .should('contain', 'SETUP INSTRUCTIONS')
+//       .and('contain', 'Configure Authentication')
+//       .and('contain', 'Database Configuration and CRUD operations')
+//       .and('contain', 'Continuous Integration & Continuous Deployment CI/CD');
+//   });
+// });
