949487e9d544b1858c6eabb262051da13cc1df6e
Eoin <eoinom@gmail.com>
2020-01-22 01:04:46 +0000

    Update API base URL

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 export default async ({ Vue }) => {
   // axios.defaults.baseURL = 'https://localhost:5001/graphql'
-  axios.defaults.baseURL = 'https://api.wealthapp.io/graphql'
+  // axios.defaults.baseURL = 'https://192.168.1.3:5001/graphql'
+  // axios.defaults.baseURL = 'https://api.wealthapp.io/graphql'
+  axios.defaults.baseURL = 'https://wealthapp-backendapi.azurewebsites.net/graphql/'
   Vue.prototype.$axios = axios
 }
